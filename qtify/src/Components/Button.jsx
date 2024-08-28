13e92dafce38da462354e07dc9a948cedc29b969
nitesh545 <hitman12457868@gmail.com>
2024-08-29 01:15:42 +0530

    search placeholder changed and other minor changes

--- qtify/src/Components/Button.jsx
+++ qtify/src/Components/Button.jsx
@@ -4,6 +4,6 @@ import styles from './Button.module.css';
 
 export default function CustomButton({text}) {
   return (
-        <Button variant="contained" className='styles.custombutton' sx={{backgroundColor: '#121212', color: '#34c94b', mx: '5rem'}}>{text}</Button>
+        <Button variant="contained"  sx={{backgroundColor: '#121212', color: '#34c94b', mx: '5rem', fontFamily: "Poppins"}}>{text}</Button>
   );
 };
--- qtify/src/Components/Search.jsx
+++ qtify/src/Components/Search.jsx
@@ -175,7 +175,7 @@ export default function Searchbar() {
               <SearchIcon />
             </SearchIconWrapper>
             <StyledInputBase
-              placeholder="Search"
+              placeholder="search"
               inputProps={{ 'aria-label': 'search' }}
             />
           </Search>

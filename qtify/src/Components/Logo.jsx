636d9a558daa7e8ae7253eecbce1db0bab1d068d
nitesh545 <hitman12457868@gmail.com>
2024-08-29 16:40:52 +0530

    logo readjustments

--- qtify/src/Components/Logo.jsx
+++ qtify/src/Components/Logo.jsx
@@ -8,7 +8,7 @@ export default function Logo() {
             src={LogoImage}
             alt="logo"
             width={67}
-            style={{ marginTop: 10, marginBottom: 10, padding: "0rem 2rem" }}
+            style={{ marginTop: 10, marginBottom: 10, marginLeft: 5 }}
         />
         // <Box component="img" src={LogoImage} alt="logo" sx={{ px: "2rem" }} />
     );

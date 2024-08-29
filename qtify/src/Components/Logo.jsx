9f3e5f57170372023c82cb8fa70436498fafa8c2
nitesh545 <hitman12457868@gmail.com>
2024-08-29 15:57:25 +0530

    fetch top albums and display modified 2

--- qtify/src/Components/Logo.jsx
+++ qtify/src/Components/Logo.jsx
@@ -2,5 +2,12 @@ import React from "react";
 import LogoImage from "../Assets/logo.png";
 
 export default function Logo() {
-  return (<img src={LogoImage} alt="logo" width={67} style={{justifyContent: 'center', padding: '1rem'}}/>);
+    return (
+        <img
+            src={LogoImage}
+            alt="logo"
+            width={67}
+            style={{ justifyContent: "center", padding: "0rem 2rem" }}
+        />
+    );
 }

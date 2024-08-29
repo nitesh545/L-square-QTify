2ca364fefdd499b8c8c22b4873fea67d5b3fb08d
nitesh545 <hitman12457868@gmail.com>
2024-08-30 04:39:15 +0530

    recenter logo 6

--- qtify/src/Components/Logo.jsx
+++ qtify/src/Components/Logo.jsx
@@ -11,9 +11,10 @@ export default function Logo() {
                 width: "67px",
                 height: "34px",
                 marginRight: "auto",
-                marginLeft: "20px",
+                marginLeft: "0px",
                 marginTop: "20px",
                 marginBottom: "20px",
+                paddingLeft: "10px",
             }}
         />
         // <Box component="img" src={LogoImage} alt="logo" sx={{ px: "2rem" }} />
